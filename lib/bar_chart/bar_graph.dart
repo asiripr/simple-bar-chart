9909e473160ae8a899c3b77bf3ad1d7656d554bb
asiripr <asiripramodaya2000@gmail.com>
2024-07-21 21:16:23 +0530

    added some decorations

--- lib/bar_chart/bar_graph.dart
+++ lib/bar_chart/bar_graph.dart
@@ -22,11 +22,61 @@ class MyBarGraph extends StatelessWidget {
       BarChartData(
         maxY: 100,
         minY: 0,
+        gridData: FlGridData(show: false),
+        borderData: FlBorderData(show: false),
+        titlesData: FlTitlesData(
+          show: true,
+          topTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          leftTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          rightTitles: AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          bottomTitles: AxisTitles(sideTitles: SideTitles(showTitles: true, getTitlesWidget: getBottomTitles))
+        ),
         barGroups: myBarData.barData.map((data) => BarChartGroupData(
           x: data.x,
-          barRods: [BarChartRodData(toY: data.y)]
+          barRods: [
+            BarChartRodData(
+              toY: data.y, 
+              color: Colors.blue.shade500,
+              width: 20,
+              borderRadius: BorderRadius.circular(5)
+            )
+          ]
           ),).toList()
       )
     );
   }
+}
+
+Widget getBottomTitles(double value, TitleMeta meta){
+  const style = TextStyle(
+    color: Colors.black,
+  );
+
+  Widget text;
+  switch (value.toInt()) {
+    case 0:
+      text = const Text('S', style: style,);
+      break;
+    case 1:
+      text = const Text('M', style: style,);
+      break;
+    case 2:
+      text = const Text('T', style: style,);
+      break;
+    case 3:
+      text = const Text('W', style: style,);
+      break;
+    case 4:
+      text = const Text('T', style: style,);
+      break;
+    case 5:
+      text = const Text('F', style: style,);
+      break;
+    case 6:
+      text = const Text('S', style: style,);
+      break;
+    default:
+      text = const Text('S', style: style,);
+  }
+  return SideTitleWidget(child: text, axisSide: meta.axisSide);
 }
