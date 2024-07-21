673f4b18469e32798fb9651f86880dad291ac052
asiripr <asiripramodaya2000@gmail.com>
2024-07-21 09:31:53 +0530

    bar chart displayed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_bar_chart/bar_chart/bar_graph.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -20,6 +21,13 @@ class _HomePageState extends State<HomePage> {
   ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          height: 200,
+          child: MyBarGraph()
+        ),
+      ),
+    );
   }
 }
